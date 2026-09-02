0655e0d6a943219ef785bee308211674a0bdcd2b
apaks <ap.akshay008@gmail.com>
2026-09-02 22:52:08 +0100

    generator eval pipeline complete

--- run_eval_pipeline.py
+++ run_eval_pipeline.py
@@ -1,13 +1,14 @@
 from src.yt_rag.evals.eval_pipeline import EvalPipeline
-from src.yt_rag.evals.retrieval_eval import RetrivalEval
+from yt_rag.evals.retriever_eval import RetrivalEval
+from src.yt_rag.evals.generator_eval import GeneratorEval
 import os
 
 if __name__ == "__main__":
-    eval_set_dir = "rag_evaluation/evaluation_set"
-    eval_sets = os.listdir(eval_set_dir)
+    # eval_set_dir = "rag_evaluation/evaluation_set"
+    # eval_sets = os.listdir(eval_set_dir)
 
-    for eval_set in eval_sets:
-        result = RetrivalEval(eval_set_name= eval_set)
+    # for eval_set in eval_sets:
+    #     result = RetrivalEval(eval_set_name= eval_set)
 
     
 
@@ -17,5 +18,12 @@ if __name__ == "__main__":
     # eval_pipeline = EvalPipeline(eval_set_dir, video_url=video_url)
 
     # eval_pipeline.run_evaluations()
+
+    # ------ Generator eval ----
+    gen_eval_pipeline = GeneratorEval()
+    gen_eval_pipeline.run(eval_set_names="7ARBJQn6QkM.json")
+
+
+
 
 
---
+++ src/yt_rag/evals/generator_eval.py
@@ -0,0 +1,130 @@
+from pathlib import Path
+import os
+import sys
+import json
+
+from src.yt_rag.components.search import RAGSearch
+from deepeval.test_case import LLMTestCase
+from deepeval.metrics import FaithfulnessMetric, AnswerRelevancyMetric
+from deepeval.evaluate import evaluate
+from deepeval.evaluate.configs import CacheConfig
+from src.yt_rag.logger import logging
+from deepeval.evaluate.types import EvaluationResult
+import pandas as pd
+from src.yt_rag.exceptions import CustomException
+
+GOLDENS_PATH = "rag_evaluation/evaluation_set/deep_eval_goldens"
+JUDGE_MODEL = "gpt-4o-mini"
+THRESHOLD = 0.7
+
+
+class GeneratorEval:
+    """This class contains components which can be used to evaluate the generator using deepeval library."""
+
+    def __init__(self, goldens_path:str = GOLDENS_PATH, judge_model_name:str = JUDGE_MODEL, threshold:float = THRESHOLD):
+        self.GOLDENS_PATH = goldens_path
+        self.judge_model_name = judge_model_name
+        self.threshold = threshold
+
+        self.eval_set_names:list = os.listdir(Path(self.GOLDENS_PATH))
+        # print(self.eval_set_names)
+
+    def run(self, eval_set_names = None):
+        eval_set_names = eval_set_names or self.eval_set_names
+        for name in self.eval_set_names:
+            test_cases = []
+
+            # load json for every set
+            eval_set_file_path = Path(self.GOLDENS_PATH, name)
+            with open(eval_set_file_path, "r") as f:
+                eval_set = json.load(f)
+                logging.info(f'[INFO] - {name} - golden dataset json file loaded')
+                # print(eval_set)
+
+            video_url = eval_set['video_url']
+            goldens = eval_set['goldens']
+
+            # create test cases
+            generator = RAGSearch(url = video_url)
+            for g in goldens:
+                context = g['context']
+                input = g['input']
+
+                # generate answer using our llm model providing the ideal context from eval set
+                answer = generator.generate_response(context = context, query=input)
+
+                test_cases.append(
+                    LLMTestCase(
+                        input= input,
+                        actual_output=answer,
+                        retrieval_context=context,
+                        # no expected answer
+                    )
+                )
+
+            # define metrics
+            metrics = [
+                FaithfulnessMetric(
+                    threshold= self.threshold,
+                    model = self.judge_model_name,
+                    include_reason= True
+                ),
+                AnswerRelevancyMetric(
+                    threshold= self.threshold,
+                    model = self.judge_model_name,
+                    include_reason= True
+                )
+            ]
+
+            # evaluate
+            results = evaluate(
+                test_cases=test_cases,
+                metrics = metrics
+            )
+            logging.info(f'[INFO] - {name} - Evaluation complete for {name}')
+            self.save_results(results = results, eval_set_name=name)
+            logging.info(f'[INFO] - {name} - Evaluation saved in csv')
+            
+
+    def save_results(self, results:EvaluationResult, eval_set_name:str):
+        rows = []
+        for test_result in results.test_results:
+            row = {
+                "eval_set_name": eval_set_name,
+                "test_name": test_result.name,
+                "model_name": None,
+                "success": test_result.success,
+                "faithfulness_score": None,
+                "faithfulness_success": None,
+                "answer_relevance_score": None,
+                "answer_relevance_success": None,
+            }
+
+            for metric_data in test_result.metrics_data:
+                row["model_name"] = metric_data.evaluation_model
+
+                if metric_data.name == "Faithfulness":
+                    row["faithfulness_score"] = metric_data.score
+                    row["faithfulness_success"] = metric_data.success
+                elif metric_data.name == "Answer Relevancy":
+                    row["answer_relevance_score"] = metric_data.score
+                    row["answer_relevance_success"] = metric_data.success
+
+            rows.append(row)
+
+        results_df = pd.DataFrame(rows)
+
+        try:
+            file_path = Path("rag_evaluation/eval_results/deep_eval_generator_results.csv")
+            if file_path.exists():
+                df = pd.read_csv(file_path)
+                # concat result df and original df
+                df = pd.concat([df, results_df], ignore_index=True)
+                df.to_csv(file_path, index = False)
+            else:
+                results_df.to_csv(file_path, index = False)
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+
+        
